56bbd07b6f966d5f09bd6e9bd483e25715c4a105
Fabrizio Fubelli <fabrizio@fubelli.org>
2020-12-14 19:23:54 +0100

    improved mongo init script

--- start-mongo.sh
+++ start-mongo.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 function init() {
+  echo "[MongoDB] checking service..."
   sleep 1.5
+  {
+    # try
+    mongo --eval 'print("OK");'
+  } || {
+    # catch
+    init
+    return
+  }
+  echo "[MongoDB] service ready"
   js_script='/usr/local/share/mongo/init.js'
   for (( i=1; ; i++ )); do
     db_name="$(eval echo "\$MONGO_DB_${i}_NAME")"
@@ -12,6 +22,7 @@ function init() {
     fi
     echo "const db_name='${db_name}'; const db_user='${db_user}'; const db_pwd='${db_pwd}'; "
     mongo --eval "const db_name='${db_name}'; const db_user='${db_user}'; const db_pwd='${db_pwd}'; " "${js_script}"
+    echo "[MongoDB] created db='${db_name}', user=''${db_user}"
   done
 }
 
